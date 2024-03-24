6a7e61b82eb4d218722bd4989c7e68dafc7cad32
sumangitron <127018473+sumangitron@users.noreply.github.com>
2024-03-24 11:58:48 +0000

    solve

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [value, setValue] = useState("");
+
+  const input = (e) => {
+    let currValue = e.target.value;
+    setValue(currValue);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <label htmlFor="name">Enter your name:</label>
+        <input type="text" id="name"  value={value} onChange={input} />
+        {value && <p>Hello {value}!</p>}
     </div>
   )
 }
